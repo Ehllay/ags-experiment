8f42c9186efcc90b55e04dd9244c6743dbc4413b
Ehllay <max.pruther@gmail.com>
2024-03-27 16:56:10 +0000

    Added applauncher

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 import Bar from "./bar/Bar.js"
+import { applauncher } from "./applauncher/applauncher.js"
 
 App.config({
     style: "./style.css",
     windows: [
         Bar(),
+        applauncher,
 
         // you can call it, for each monitor
         // Bar(0),
