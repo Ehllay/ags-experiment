35871024fcfbf68ed4c701fe9a23b0e9a550b12b
Ehllay <max.pruther@gmail.com>
2024-03-27 16:00:41 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+import Bar from "./bar/Bar.js"
+
+App.config({
+    style: "./style.css",
+    windows: [
+        Bar(),
+
+        // you can call it, for each monitor
+        // Bar(0),
+        // Bar(1)
+    ],
+})
+
+export {    }
