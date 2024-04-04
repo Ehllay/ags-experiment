6f19e56b8eff8b3450de28da4f61c88d1dc2cf56
Ehllay <max.pruther@gmail.com>
2024-04-04 16:42:59 +0100

    Fixed css autoreload

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+import Gio from "gi://Gio";
+
 import Bar from "./bar/Bar.js"
 import { NotificationPopups } from "./notifications/Notifications.js"
 import { applauncher } from "./applauncher/applauncher.js"
 
 App.config({
-    style: "./styles/main.scss",
+    style: "./styles/main.css",
     windows: [
         Bar(0),
         //NotificationPopups(),
@@ -16,21 +18,21 @@ App.config({
 
 const applyScss = () => {
   // Compile scss
-  exec(`sassc ${App.configDir}/styles/main.scss ${App.configDir}/styles/style.css`);
-  console.log("Scss compiled");
+  //Utils.exec(`sassc ${App.configDir}/styles/main.scss ${App.configDir}/styles/style.css`);
+  //console.log("Scss compiled");
 
   // Apply compiled css
   App.resetCss();
-  App.applyCss(`${App.configDir}/styles/style.css`);
-  console.log("Compiled css applied");
+  App.applyCss(`${App.configDir}/styles/main.css`);
+  //console.log("Compiled css applied");
 };
 
-Utils.monitorFile(`${App.configDir}/styles`, (_, eventType) => {
+Utils.monitorFile(`${App.configDir}/styles/`, (_, eventType) => {
   if (eventType === Gio.FileMonitorEvent.CHANGES_DONE_HINT) {
     applyScss();
   }
 });
 
-//applyScss();
+applyScss();
 
 export {    }
